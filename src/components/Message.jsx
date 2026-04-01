5e15d4953bbfad0772b505c3e46d4b413870784b
Akorlldas <m3058m@qq.com>
2026-04-01 18:05:12 +0800

    Chinese Simplify in list

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,16 @@
 import styles from "./Message.module.css";
 
 function Message({ message }) {
+  let message_cn = message === "city" ? "城市" : "国家";
+
   return (
-    <p className={styles.message}>Add by clicking on a {message} on the map</p>
+    <>
+      <br />
+      <p className={styles.message}>
+        Add by clicking on a {message} on the map
+      </p>
+      <p className={styles.message}>通过点击地图添加{message_cn}</p>
+    </>
   );
 }
 
